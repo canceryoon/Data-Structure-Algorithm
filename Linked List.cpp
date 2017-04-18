d1933a7f8134bddf60099a3d277eb4b04ec51def
canceryoon96 <canceryoon96@gmail.com>
2017-04-19 00:15:20 +0900

    Single Linked List USAGE: Input Num - 1(insert) 2(select) 3(Search) 4(update) 5(delete) 6(End Program) TODO: Multi Search, Delete Flag. It control by Bit.

---
+++ Linked List.cpp
@@ -0,0 +1,156 @@
+#include<iostream>
+#define SEARCH_TRUE 0x1
+#define DELETE_TRUE 0x2
+using namespace std;
+struct node
+{
+    int val;
+    node* next;
+};
+
+node* start;
+
+void insert(int x)
+{
+    node* t = start;
+    if( !start )
+    {
+        node* _next = new node;
+        _next->val = x;
+        _next->next = NULL;
+        start = _next;
+    }
+    else
+    {
+        while(t->next)
+        {
+            t = t->next;
+        }
+        node* _next = new node;
+        _next->val = x;
+        _next->next = NULL;
+        t->next = _next;
+    }
+}
+
+void select()
+{
+    int count = 1;
+    if(!start) cout << "No data" << endl;
+    node* t = start;
+    while(t)
+    {
+        cout << count << " node : " << t->val << endl;
+        count++;
+        t = t->next;
+    }
+}
+
+void search(int x)
+{
+    if(!start) cout << "No Node" << endl;
+    int count = 1;
+    bool flag;
+    node* t = start;
+    while(t)
+    {   
+        if(t->val == x)
+        {
+            cout << count << " node has " << t->val << endl;
+            flag = SEARCH_TRUE;
+        }
+        count++;
+        t = t->next;
+    }
+    if( flag ^ SEARCH_TRUE )
+    {
+        cout << "No data in node: " << x << endl;
+    }
+
+}
+
+void remove(int x)
+{
+    if(!start) cout << "No Node" << endl;
+    node* t = start;
+    node* p;
+    while(t)
+    {   
+        if(t->val == x)
+        {
+            p->next = t->next;
+            t = NULL;
+            delete t;
+            t = p->next;
+            continue;
+        }
+        p=t;
+        t=t->next;
+    }
+}
+
+void update(int _x, int x)
+{
+    if(!start) cout << "No Node" << endl;
+    int count = 1;
+    node* t = start;
+    while(t)
+    {   
+        if(t->val == _x)
+        {
+            t->val = x;
+            return ;
+        }
+        count++;
+        t = t->next;
+    }
+    cout << "No data in node: " << _x << endl;   
+}
+
+int main()
+{
+    int n,x,_x;
+    do{
+        cout << "1. insert" << endl << "2. select" << endl << "3. Search" << endl << "4. update" << endl << "5. delete" << endl << "6. End" << endl;
+        cin >> n;
+        switch(n)
+        {
+            case 1: 
+                cout << "Insert Num" << endl; 
+                cin >> x;
+                insert(x);
+                break;
+            case 2: 
+                cout << "Select Num" << endl; 
+                select();
+                break;
+            case 3: 
+                cout << "Search Num" << endl; 
+                cin >> x;
+                search(x);
+                break;
+            case 4: 
+                cout << "Update A Num to B Num" << endl; 
+                cin >> _x;
+                cin >> x;
+                update(_x, x); 
+                break;
+            case 5: 
+                cout << "Delete" << endl; 
+                cin >> x;
+                remove(x);
+                break;
+        }
+    }while(n!=6);
+    cout << "End:" << endl;
+    
+    //delete node
+    node* t;
+    while(start)
+    {
+        t = start->next;
+        delete start;
+        start = t;
+    }
+    return 1;
+}
