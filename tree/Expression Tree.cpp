5c176f5b68ab3e69c651c1c4cb21b8b64486baff
canceryoon <photo9609@gmail.com>
2017-05-11 23:20:12 +0900

    Add Expression Tree

--- tree/Expression Tree.cpp
+++ tree/Expression Tree.cpp
@@ -1,1 +1,116 @@
 #include "Expression Tree.h"
+
+ETNode* ET_CreateNode( char _data )
+{
+    ETNode* nNode = (ETNode*)malloc(sizeof(ETNode));
+    nNode->left = NULL;
+    nNode->right = NULL;
+    nNode->data = _data;
+    return nNode;
+}
+
+void ET_DestoryNode( ET_Node* dNode )
+{
+    free(dNode);
+}
+
+void ET_DestoryTree( ET_Node* head )
+{
+    if(head)
+    {   
+        ET_DestoryTree(head->left);
+        ET_DestoryTree(head->right);
+        ET_DestoryNode(head);
+    }
+    return ;
+}
+
+void ET_PreorderPrintTree( ETNode* node )
+{
+    if(node)
+    {
+        std::cout << " " << node->data ;
+        ET_PreorderPrintTree(node->left);
+        ET_PreorderPrintTree(node->right);
+    }
+    return ;
+}
+
+void ET_InorderPrintTree( ETNode* node )
+{
+    if(node)
+    {
+        std::cout << "(" ;
+        ET_InorderPrintTree(node->left);
+        std::cout << node->data ;
+        ET_InorderPrintTree(node->right);
+        std::cout << ")" ;
+    }
+    return ;
+}
+
+void ET_PostorderPrintTree( ETNode* node )
+{
+    if(node)
+    {
+        ET_PostorderPrintTree(node->left);
+        ET_PostorderPrintTree(node->right);
+        std::cout << node->data ;
+    }
+    return ;
+}
+
+void ET_BuildExpressionTree( char* postFixNode, ETNode** node )
+{
+    int len  = std::strlen(postFixNode);
+    char token = postFixNode[len-1];
+    postFixNode[len-1] = '\0';
+
+    switch(token)
+    {
+        case '+' : 
+        case '-' : 
+        case '*' : 
+        case '/' :  (*node) = ET_CreateNode( token );
+                    ET_BuildExpressionTree( postFixNode , &(*node)->right );
+                    ET_BuildExpressionTree( postFixNode , &(*node)->left );
+                    break;
+        default  :  (*node) = ET_CreateNode( token );
+                    break;
+    }
+}
+
+double ET_Evaluate( ETNode* tree );
+{
+    char tmp[2];
+
+    double left = 0;
+    double right = 0;
+    double ret = 0;
+
+    if(tree)
+    {
+        switch( tree->data )
+        {
+            case '+' : 
+            case '-' : 
+            case '*' : 
+            case '/' :  left = ET_Evaluate( tree->left );
+                        right = ET_Evaluate( tree->right );
+                        switch( tree->data )
+                        {
+                            case '+': ret = left + right; break;
+                            case '-': ret = left - right; break;
+                            case '*': ret = left * right; break;
+                            case '/': ret = left / right; break;
+                        }
+                        break;
+            default  :  memset(tmp, 0, sizeof(tmp));
+                        tmp[0] = tree->data;
+                        ret = std::atof(tmp);
+                        break; 
+        }
+        return ret;
+    }
+    return 0;
+}
--- tree/Expression Tree.h
+++ tree/Expression Tree.h
@@ -1,4 +1,22 @@
 #ifndef EXPRESSION_TREE_H
 #define EXPRESSION_TREE_H
 
+#include<iostream>
+struct ETNode
+{
+    ETNode* left;
+    ETNode* right;
+    char data;
+};
+
+ETNode* ET_CreateNode( char ndata );
+void ET_DestoryNode( ETNode* dNode );
+void ET_DestoryTree( ETNode* head );
+
+void EX_PreorderPrintTree( ETNode* node );
+void ET_InorderPrintTree( ETNode* node );
+void ET_PostorderPrintTree( ETNode* node );
+void ET_BuildExpressionTree( char* postFixNode, ETNode** node );
+double ET_Evaluate( ETNode* tree );
+
 #endif
