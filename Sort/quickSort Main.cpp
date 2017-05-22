22aa0447fdd1184796c683eafddc55a3950469e5
canceryoon <photo9609@gmail.com>
2017-05-22 19:10:19 +0900

    Add Quick Sort and Partition Function. TODO: Need bugfix quick sort: sefault error 11

--- Sort/quickSort Main.cpp
+++ Sort/quickSort Main.cpp
@@ -5,7 +5,7 @@ int main()
 {
     int arr[] = {4,1,5,7,3,6,0,9};
     int len = sizeof(arr)/sizeof(arr[0]);
-    quicksort(&arr[0], len);
+    quicksort(&arr[0], 0, len-1);
     for(int i = 0 ; i < len; i++ )
         std::cout << arr[i] << " ";
 }
--- Sort/quickSort.h
+++ Sort/quickSort.h
@@ -1,9 +1,44 @@
-#ifndef
+#ifndef QUICK_SORT_H
 #define QUICK_SORT_H
 
-void quicksort(int* arr, int len)
-{
+#include <iostream>
 
+void swap(int* a, int* b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = *a;
+}
+
+int partition(int* arr, int l, int r)
+{
+    int f = l++;
+    int p = arr[f];
+
+    while(l <= r)
+    {
+        while(arr[l++] <= p);
+        while(arr[r--] > p);
+
+        if(l < r) r--;
+
+        if(l < r) swap(&arr[l], &arr[r]);
+        else break;
+    }
+    
+    swap(&arr[f], &arr[r]);
+    return r;   
+}
+
+void quicksort(int* arr, int l, int r)
+{
+    if( l < r )
+    {
+        int idx = partition(arr, l, r);
+        std::cout << "after partition " << idx << std::endl;
+        quicksort(arr, l, idx-1);
+        quicksort(arr, idx+1, r);
+    }
 }
 
 #endif
