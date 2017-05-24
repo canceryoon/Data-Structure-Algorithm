ccdfb716f9f6961b92e94ed87b9437a90cbc396a
canceryoon <photo9609@gmail.com>
2017-05-24 20:38:15 +0900

    Complete My Binary Search. Use c standard library sort: Qsort.

--- Search/BinarySearch1 Main.cpp
+++ Search/BinarySearch1 Main.cpp
@@ -1,13 +1,30 @@
 #include<iostream>
 #include "BinarySearch1.h"
 
+inline static int compareFunc(const void* elem1, const void* elem2)
+{
+    int* e1 = (int*)elem1;
+    int* e2 = (int*)elem2;
+    if( *e1 > *e2 ) return 1;
+    else if( *e1 < * e2 ) return -1;
+    return 0;
+}
+
 int main()
 {
     int dataset[] = {1,4,3,9,7,0,5,8,2};
-    int target = 2;
+    int target ;
     int len = sizeof(dataset)/sizeof(dataset[0]);
     int ret;
+    std::cout << "Enter the search num: ";
+    std::cin >> target;
 
+    qsort( (void*)dataset, len, sizeof(int), compareFunc);
     ret = binarysearch(&dataset[0], len, target);
+    
+    if( ret == target )
+        std::cout << "Found the " << ret << std::endl;
+    else std::cout << "Data Set does not has " << ret << std::endl;
+    
     return ret;
 }
--- Search/BinarySearch1.h
+++ Search/BinarySearch1.h
@@ -3,9 +3,23 @@
 
 int binarysearch(int* arr, int size, int target)
 {
-    int ret = -1;
+    int l,r,m;
+    
+    l = 0;
+    r = size - 1;
+    while(l <= r)
+    {
+        m = (l + r) / 2;
+        if( arr[m] == target )
+            return arr[m];
+        
+        else if( arr[m] > target )
+            r = m - 1;
+        else
+            l = m + 1;
+    }
 
-    return ret;
+    return -1;
 }
 
 #endif
