66bdb866e75a442d29d179d9eaccdd55e3e99305
canceryoon <photo9609@gmail.com>
2017-05-28 16:13:48 +0900

    Create Binary Search Tree . TODO: Remove node.

--- Search/BinarySearchTree.cpp
+++ Search/BinarySearchTree.cpp
@@ -1,1 +1,89 @@
 #include "BinarySearchTree.h"
+
+NODE* createNode(int _data)
+{
+    NODE* node = (NODE*)malloc(sizeof(NODE());
+    node->data = _data;
+    node->right = NULL;
+    node->left = NULL;
+
+    return node;
+}
+
+void destroyNode(NODE* rnode)
+{
+    free(node);
+}
+
+void destroyTree(NODE* tree)
+{
+    if(tree->left != NULL)
+        destroyTree(tree->left);
+    
+    if(tree->right != NULL)
+        destroyTree(tree->right);
+    
+    tree->left = NULL;
+    tree->right = NULL;
+    destroyNode(tree);
+}
+
+NODE* searchNode(NODE* tree, int target)
+{
+    if(tree == NULL)
+        return NULL;
+
+    if(tree->data == target)
+        return tree;
+    
+    if(tree->data < target)
+        return searchNode(tree->left);
+    else return searchNode(tree->right);
+
+    std::cout << "No data found: " << target << std::endl;
+    return NULL;
+}
+
+NODE* searchMinNode(NODE* tree)
+{
+    if(tree == NULL)
+    {
+        std::cout << "Input Tree is NULL" << std::endl;
+        return NULL;
+    }
+
+    if(tree->left != NULL )
+        return searchMinNode(tree->left);
+        
+    return tree;
+}
+
+void insertNode(NODE* tree, NODE* child)
+{
+    if(tree == NULL)
+    {
+        std::cout << "Input Tree is NULL" << std::endl;
+        return NULL;
+    }
+
+    if(tree->data > child->data)
+    {
+        if(tree->left)
+            insertNode(tree->left, child);
+        else
+            tree->left = child; 
+    }
+    else if(tree->data < child->data)
+    {
+        if(tree->right)
+            insertNode(tree->right, child);
+        else
+            tree->right = child; 
+    }
+}
+
+NODE* removeNode(NODE* tree, NODE* parent, int rdata)
+{
+    
+    return tree;
+}
--- Search/BinarySearchTree.h
+++ Search/BinarySearchTree.h
@@ -1,4 +1,24 @@
 #ifndef BINARY_SEARCH_TREE_H
 #define BINARY_SEARCH_TREE_H
 
+#include<iostream>
+
+struct NODE{
+    int data;
+    NODE* left;
+    NODE* right;
+};
+
+NODE* createNode(int _data);
+void destroyNode(NODE* rnode);
+void destroyTree(NODE* tree);
+
+void printNode(NODE* tree);
+void insertNode(NODE* tree, NODE* child);
+NODE* removeNode(NODE* tree, NODE* parent, int rdata);
+NODE* searchNode(NODE* tree, int target);
+NODE* searchMinNode(NODE* tree);
+
+
+
 #endif
