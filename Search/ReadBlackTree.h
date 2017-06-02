ac0c3d4cf91fcf012e2b68766895800b7fa09fcf
canceryoon <photo9609@gmail.com>
2017-06-02 22:17:14 +0900

    Add function define

--- Search/ReadBlackTree.h
+++ Search/ReadBlackTree.h
@@ -13,6 +13,23 @@ struct RBTree{
     RBTree* right;
     RBTree* parent;
     Color color;
-};
+}RBNode;
+
+RBNode* createRBNode(int data);
+void destroyRBNode(RBNode* rnode);
+void destroyRBTree(RBNode* tree);
+
+RBNode* searchRBNode(RBNode* tree, int target);
+RBNode* searchMinRBNode(RBNode* tree);
+RBNode* removeNode(RBNode** tree, int data);
+
+void insertNode(RBNode** tree, RBNode* nNode);
+void insertNodeHelper(RBNode** tree, RBNode* nNode);
+void rebuildAfterInsert(RBNode** tree, NODE* x);
+void rebuildAfterRemove(RBNode** tree, RBNode* x);
+
+void printTree(RBNode* node, int depth, int blackCount);
+void rotateLeft(RBNode** root, RBNode* parent);
+void rotateRight(RBNode** root, RBNode* parent);
 
 #endif
