3e6e6d666db2ecaaaa05790d3e9354e797a88cf5
canceryoon <photo9609@gmail.com>
2017-06-11 15:27:30 +0900

    Create createNode, getParent, getLeftChild

--- PriorityQueue/Heap.cpp
+++ PriorityQueue/Heap.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 #include "Heap.h"
+
+NODE* HEAP::createNode(int size)
+{
+    NODE* nNode = (NODE*)malloc(sizeof(NODE) * size);
+    return nNode;
+}
+
+int HEAP::getParent(int idx)
+{
+    return (int)((idx-1)/2);
+}
+
+int HEAP::getLeftChild(int idx)
+{
+    return (2 * idx) + 1;
+}
