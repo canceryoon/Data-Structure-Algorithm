9fea2cc839496e0920137ce75470928aa9f6a7a2
canceryoon <photo9609@gmail.com>
2017-06-12 17:41:03 +0900

    create print node func and swap func

--- PriorityQueue/Heap.cpp
+++ PriorityQueue/Heap.cpp
@@ -16,3 +16,23 @@ int HEAP::getLeftChild(int idx)
 {
     return (2 * idx) + 1;
 }
+
+void HEAP::swapNode(int idx1, int idx2)
+{
+    int cpsize = sizeof(NODE);
+    NODE* tmp = (NODE*)malloc(cpsize);
+
+    memcpy(tmp, &h->node[idx1], cpsize);
+    memcpy(&h->node[idx1] , &h->node[idx2], cpsize);
+    memcpy(&h->node[idx2], tmp, cpsize);
+
+    tree(tmp);
+}
+
+void HEAP::printHeap()
+{
+    for(unsigned int i = 0; i < capacity; i++)
+    {
+        std::cout << node[i].data << " " ;
+    }
+}
