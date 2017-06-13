607ec2f677d34bf0dab0f440bd3a881cc77f51f6
canceryoon <photo9609@gmail.com>
2017-06-13 20:10:05 +0900

    Create Heap Insert

--- PriorityQueue/Heap.cpp
+++ PriorityQueue/Heap.cpp
@@ -22,9 +22,9 @@ void HEAP::swapNode(int idx1, int idx2)
     int cpsize = sizeof(NODE);
     NODE* tmp = (NODE*)malloc(cpsize);
 
-    memcpy(tmp, &h->node[idx1], cpsize);
-    memcpy(&h->node[idx1] , &h->node[idx2], cpsize);
-    memcpy(&h->node[idx2], tmp, cpsize);
+    memcpy(tmp, &node[idx1], cpsize);
+    memcpy(&node[idx1] , &node[idx2], cpsize);
+    memcpy(&node[idx2], tmp, cpsize);
 
     tree(tmp);
 }
@@ -35,4 +35,27 @@ void HEAP::printHeap()
     {
         std::cout << node[i].data << " " ;
     }
+}
+
+void insertNode(int _data)
+{
+    int cur_pos = usedSize;
+    int parent_pos = getParent(cur_pos);
+
+    if(capacity == usedSize)
+    {
+        capacity *= 2;
+        node = (NODE*)realloc(node, sizeof(NODE) * capacity);
+    }
+
+    node[cur_pos].data = _data;
+
+    while(cur_pos > 0 &&  node[cur_pos].data < node[parent_pos].data)
+    {
+        swapNode(cur_pos, parent_pos);
+        cur_pos = parent_pos;
+        parent_pos = getParent(cur_pos);
+    }
+
+    usedSize++;
 }
