3e4f11448faa45b9ce8da950522a3cc6d19afcd8
canceryoon <photo9609@gmail.com>
2017-06-14 19:54:22 +0900

    Create deleteMinNode

--- PriorityQueue/Heap.cpp
+++ PriorityQueue/Heap.cpp
@@ -37,7 +37,7 @@ void HEAP::printHeap()
     }
 }
 
-void insertNode(int _data)
+void HEAP::insertNode(int _data)
 {
     int cur_pos = usedSize;
     int parent_pos = getParent(cur_pos);
@@ -58,4 +58,45 @@ void insertNode(int _data)
     }
 
     usedSize++;
+}
+
+NODE* deleteMinNode()
+{
+    int parent_pos = 0;
+    int lpos = 0;
+    int rpos = 0;
+
+    memset(node[0], 0, sizeof(NODE));
+    usedSize -= 1;
+
+    swapNode(0, usedSize);
+    lpos = getLeftChild(0);
+    rpos = lpos + 1;
+
+    while(1)
+    {
+        int selectChild = 0;
+        if(lpos >= usedSize) break;
+
+        if(rpos >= usedSize)    
+            selectChild = rpos;
+        else    
+            selectChild = (node[lpos].data > node[rpos].data)? rpos : lpos ;
+        
+        if(node[selectChild].data < node[parent_pos].data)
+        {
+            swapNode(parent_pos, selectChild);
+            parent_pos = selectChild;
+        }
+        else
+            break;
+
+        lpos = getLeftChild(parent_pos);
+        rpos = lpos + 1;
+    }
+    if(usedSize < capacity/2)
+    {
+        capacity /= 2;
+        node = (NODE*)realloc(node, (sizeof(NODE)*capacity));
+    }
 }
