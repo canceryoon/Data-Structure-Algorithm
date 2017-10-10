9a4bd43614e1aca56fca81c6ebb489c16c64a936
canceryoon <sjyoon@pentasecurity.com>
2017-10-10 16:40:06 +0900

    Implements Priority Queue Using Heap  1. createNode  2. destroy  3. enQueue
     Not using malloc/free. use new/delete

--- PriorityQueue/PriorityQueue.cpp
+++ PriorityQueue/PriorityQueue.cpp
@@ -1,2 +1,36 @@
 #include <iostream>
 #include "PriorityQueue.h"
+
+void HEAP::createNode( int _capacity )
+{
+  usedSize = 0;
+  capacity = _capacity;
+  node = new NODE[capacity];
+}
+
+void HEAP::destroy()
+{
+  delete []node;
+  usedSize = 0;
+  capacity = 0;
+}
+
+void HEAP::enQueue( int _data )
+{
+  int curPos = usedSize;
+  int parentPos = getParent( curPos );
+  if( usedSize != capacity )
+  {
+    node[curPos]._data = _data; 
+    while( curPos > 0 && node[curPos]._data < node[parentPos]._data )
+    {
+      swapNode( curPos, parentPos );
+      curPos = parentPos;
+      parentPos = getParent( curPos );
+    }
+    usedSize++;
+    return ;
+  }
+
+  std::cout << "Full Heap. (" << usedSize << ")" << std::endl;
+}
--- PriorityQueue/PriorityQueue.h
+++ PriorityQueue/PriorityQueue.h
@@ -3,31 +3,30 @@
 
 #include <iostream>
 
+/*
+ * implement Heap Class
+ */
+
 struct NODE{
-    int data;
-    int priority;
+    int _data;
 };
 
-class PQ{
+class HEAP{
 private: 
     NODE* node;
     int capacity;
     int usedSize;
 
 public:
-    PQ(int _capacity){ createNode(_capacity); }
+    HEAP(int _capacity){ createNode(_capacity); }
     void createNode(int _capacity);
     void destroy();
-    void enQueue();
+    void enQueue( int _data );
     void deQueue();
+    void PrintHeap();
 
 protected:
-    void setCapacity(int _capacity);
-    void setUsedSize(int _usedSize);
     void swapNode(int idx1, int idx2);
-
-    int getCapacity();
-    int setUsedSize();
     int getParent(int idx);
     int getLeftChild(int idx);
 };
