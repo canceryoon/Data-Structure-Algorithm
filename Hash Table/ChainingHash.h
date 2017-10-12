37721722610b8d55f408e5886b613560aea72246
canceryoon <photo9609@gmail.com>
2017-10-12 14:21:44 +0900

    define Chaining hash class

--- Hash Table/ChainingHash.h
+++ Hash Table/ChainingHash.h
@@ -1,4 +1,33 @@
 #ifndef CHAINING_HASH_H
 #define CHAINING_HASH_H
 
+typedef struct node
+{
+  char* _key;
+  char* _value;
+
+  struct node* _next;
+}Node;
+
+typedef Node* List;
+
+class ChainHash
+{
+  int tableSize;
+  List* CH;
+
+public:
+  ChainHash(int _size): tableSize(_size);
+  ~ChainHash();
+
+  int setCH( char* _key, char* _value );
+  char* getCH( char* _key );
+
+private:
+  int calculateKey( char* _key );
+  Node* createNode( char* _key, char* _value );
+  void destroyNode( Node* _deNode );
+  void destroyList( List _deList );
+}
+
 #endif
