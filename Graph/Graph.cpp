85a4c259cbb64ddcb25211a3c5416d23a528b66b
canceryoon <photo9609@gmail.com>
2017-10-17 19:35:41 +0900

    add functions
    graph create/destroy
    vertex create/destroy
    edge create/destroy

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -1,3 +1,45 @@
 #include "Graph.h"
 
+myGraph* createGraph()
+{
+  myGraph* graph = (myGraph*)malloc(sizeof(myGraph));
+  graph->vertices = NULL;
+  graph->vertexCnt = 0;
 
+  return graph;
+}
+
+void destroyGraph( myGraph* G )
+{
+  while( G->vertices != NULL )
+  {
+    myVertex* v = G->vertices->next;
+    destroyVertex(G->vertices);
+    G->vertices = v;
+  }
+  free(G);
+}
+
+myVertex* createVertex( int _data )
+{
+  myVertex* v = (myVertex*)malloc(sizeof(myVertex));
+
+  v->data = _data;
+  v->next = NULL;
+  v->adjacencyList = NULL;
+  v->visited = NotVisited;
+  v->idx = -1;
+
+  return v;
+}
+
+void destroyVertex( myVertex* v )
+{
+  while( v->adjacencyList != NULL )
+  {
+    myEdge* e = v->adjacencyList->next;
+    destroyEdge(v-adjacencyList);
+    v->adjacencyList = e;
+  }
+  free(v);
+}
