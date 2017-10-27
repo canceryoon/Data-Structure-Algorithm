c15fc0fd9a7c2816033e45d9c888c829cb9ffd6b
canceryoon <photo9609@gmail.com>
2017-10-27 16:05:19 +0900

    ADD TopologicalSort Function

---
+++ Graph/TopologicalSort.cpp
@@ -0,0 +1,35 @@
+#include "TopologicalSort.h"
+
+using namespace std;
+
+void TopologicalSort( myVertex* v, Node* list)
+{
+	while( v != NULL && v->visited != Visited )
+	{
+		TS_DFS(v, list);
+		v = v->next;
+	}
+}
+
+void TS_DFS( myVertex* v, Node* list)
+{
+	Node* newHead = NULL;
+	myEdge* e = NULL;
+	
+	v->visited = Visited;
+	e = v->adjacencyList;
+	
+	while( e != NULL )
+	{
+		if( e->to != NULL && e->to->visited != Visited )
+			TS_DFS(e->to, list);
+			
+		e = e->next;
+	}
+	
+	cout << v->data << endl;
+	newHead = (Node*)malloc(sizeof(Node));
+	newHead->val = v;
+	newHead->next = NULL;
+	insertNHead(list, newHead);
+}
---
+++ Graph/TopologicalSort.h
@@ -0,0 +1,10 @@
+#ifndef TOPOLOGICALSORT_H
+#define TOPOLOGICALSORT_H
+
+#include "Graph.h"
+#include "LinkedList.h"
+
+void TopologicalSort( myVertex* v, Node* list);
+void TS_DFS( myVertex* v, Node* list);
+
+#endif
