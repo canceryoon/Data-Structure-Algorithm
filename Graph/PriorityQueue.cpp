e47958c14770338e69a52f68e06cf4e55a1df9a1
canceryoon <photo9609@gmail.com>
2017-10-30 23:15:59 +0900

    Add PriorityQueue for MinimumSpanning Tree

---
+++ Graph/PriorityQueue.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include "PriorityQueue.h"
+
+void HEAP::createNode( int _capacity )
+{
+  usedSize = 0;
+  capacity = _capacity;
+  node = (NODE*)malloc(sizeof(NODE)*(capacity)); 
+}
+
+void HEAP::destroy()
+{
+  free(node);
+}
+
+void HEAP::enQueue( NODE _node )
+{
+  int curPos = usedSize;
+  int parentPos = getParent( curPos );
+  if( usedSize != capacity )
+  {
+    node[curPos] = _node; 
+    while( curPos > 0 && node[curPos]._priority < node[parentPos]._priority )
+    {
+      swapNode( curPos, parentPos );
+      curPos = parentPos;
+      parentPos = getParent( curPos );
+    }
+    usedSize++;
+    return ;
+  }
+
+  std::cout << "Full Heap. (" << usedSize << ")" << std::endl;
+}
+
+void HEAP::swapNode( int idx1, int idx2 )
+{
+  NODE* tmp = (NODE*)malloc(sizeof(NODE));
+  memcpy(tmp, &node[idx1], sizeof(NODE) );
+  memcpy(&node[idx1], &node[idx2], sizeof(NODE) );
+  memcpy(&node[idx2], tmp, sizeof(NODE) );
+  free(tmp);
+}
+
+int HEAP::getLeftChild( int idx )
+{
+  return (2*idx) + 1;
+}
+
+int HEAP::getParent( int idx )
+{
+  return (int)((idx-1)/2);
+}
+
+void HEAP::deQueue()
+{
+  int parentPos = 0;
+  int LeftPos = 0;
+  int RightPos = 0;
+  
+  memset(&node[0], 0, sizeof(NODE));
+  usedSize--;
+  swapNode( 0, usedSize );
+  LeftPos = getLeftChild( 0 );
+  RightPos = LeftPos + 1;
+  
+  while(1)
+  {
+    int selectChild = 0;
+    
+    if( LeftPos >= usedSize ) break;
+    if( RightPos >= usedSize ) 
+    {
+      selectChild = LeftPos;
+    }
+    else
+    {
+      if( node[LeftPos]._priority > node[RightPos]._priority )
+	selectChild = RightPos;
+      else
+	selectChild = LeftPos;
+    }
+
+    if( node[selectChild]._priority < node[parentPos]._priority )
+    {
+      swapNode( parentPos, selectChild );
+      parentPos = selectChild;
+    }
+    else
+      break;
+
+    LeftPos = getLeftChild( parentPos );
+    RightPos = LeftPos + 1;
+  }
+}
+
+void HEAP::PrintHeap()
+{
+  int i = 0;
+  for( i = 0; i < usedSize; i++ )
+  {
+    std::cout << node[i]._priority; 
+    printf(" [%s] ", node[i]._data );
+  }
+  std::cout << " " << std::endl;
+}
+
+bool HEAP::isEmpty()
+{
+	return (usedSize == 0);
+}
---
+++ Graph/PriorityQueue.h
@@ -0,0 +1,35 @@
+#ifndef PRIORITY_QUEUE_H
+#define PRIORITY_QUEUE_H
+
+#include <iostream>
+
+/*
+ * implement Heap Class
+ */
+
+struct NODE{
+    int _priority;
+    void* _data;
+};
+
+class HEAP{
+private: 
+    NODE* node;
+    int capacity;
+    int usedSize;
+
+public:
+    HEAP(int _capacity){ createNode(_capacity); }
+    void createNode(int _capacity);
+    void destroy();
+    void enQueue( NODE _node );
+    void deQueue();
+    void PrintHeap();
+		bool isEmpty();
+protected:
+    void swapNode(int idx1, int idx2);
+    int getParent(int idx);
+    int getLeftChild(int idx);
+};
+
+#endif
