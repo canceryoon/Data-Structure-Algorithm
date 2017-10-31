b9c5ff4a3d35c8e624407307bbbee2781b118ca1
canceryoon <photo9609@gmail.com>
2017-10-31 19:03:10 +0900

    Add Minimum Spanning Tree: Prim algo

---
+++ Graph/MST.cpp
@@ -0,0 +1,10 @@
+#include "MST.h"
+
+void prim( Graph *g, myVertex *startV, Graph *mst )
+{
+  int i = 0;
+  NODE *startNode = { 0, startV };
+  HEAP pq(10);
+  myVertex *currentV = NULL;
+  myEdge *currentE = NULL;
+}
---
+++ Graph/MST.h
@@ -0,0 +1,9 @@
+#ifndef MST_H
+#define MST_H
+
+#include "Graph.h"
+#include "PriorityQueue.h"
+
+void prim( Graph *g, myVertex *startV, Graph *mst );
+
+#endif
