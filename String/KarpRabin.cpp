1317f6d937911304a8f690ce060b9c0e7cd7c34f
canceryoon <photo9609@gmail.com>
2017-12-06 11:34:22 +0900

    Implements hash / rehash
    bugfix karprabin function

--- String/KarpRabin.cpp
+++ String/KarpRabin.cpp
@@ -10,17 +10,17 @@ int KarpRarbin( char *text, char *pattern, int st)
   int j = 0;
 
   int patternLen = strlen(pattern);
-  int textlen = strlen(text);
+  int textLen = strlen(text);
 
   int coefficient = pow(2, patternLen - 1);
-  cout << "patternLen: " << pattenLen << endl;
+  cout << "patternLen: " << patternLen << endl;
 
-  int hashText = hash(text, pattenLen);
+  int hashText = hash(text, patternLen);
   int hashPattern = hash(pattern, patternLen);
 
   for(; i <= textLen; i++)
   {
-    hashText = rehash(text, i, patternLen - 1, hashText, coefficien);
+    hashText = rehash(text, i, patternLen - 1, hashText, coefficient);
     if( hashPattern == hashText )
     {
       for( j = 0; j < patternLen; j++)
@@ -34,3 +34,20 @@ int KarpRarbin( char *text, char *pattern, int st)
 
   return -1;
 }
+
+inline int hash( char *str, int _size )
+{
+  int i = 0;
+  int hashval = 0;
+  
+  for(; i < _size; i++)
+    hashval = str[i] + (hashval << 1);
+
+  return hashval;
+}
+
+inline int rehash( char *str, int st, int _size, int hashPrev, int coefficient )
+{
+  return (st == 0)? hashPrev : (str[st + _size] + ( (hashPrev - coefficient * str[st-1]) << 1 ));
+}
+
