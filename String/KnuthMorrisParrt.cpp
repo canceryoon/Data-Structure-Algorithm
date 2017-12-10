1c8eb080510919da4d88ae229ae9ef24031b42b4
unknown <photo9609@gmail.com>
2017-12-10 23:35:20 +0900

    Implement KMP function.

--- String/KnuthMorrisParrt.cpp
+++ String/KnuthMorrisParrt.cpp
@@ -16,6 +16,25 @@ void preprocess(char *pt, int ptLen, int *border)
 
 int knutmorrispratt(char *text, int textLen, int st, char *pt, int ptLen)
 {
+	int i = st;
+	int j = 0;
+	int pos = -1;
+	int *border = (int*)malloc(sizeof(int) * (ptLen + 1));
+	preprocess(pt, ptLen, border);
 
-	return -1;
+	while (i < textLen)
+	{
+		if (j >= 0 && (text[i] != pt[j]))
+			j = border[j];
+		i += 1;
+		j += 1;
+
+		if (j == ptLen)
+		{
+			pos = i - j;
+			break;
+		}
+	}
+	free(border);
+	return pos;
 }
