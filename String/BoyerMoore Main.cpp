f43ec69c11a42b4a8022b6a771cf6991ef821d68
canceryoon <photo9609@gmail.com>
2018-02-01 14:39:10 +0900

    Add Main test

---
+++ String/BoyerMoore Main.cpp
@@ -0,0 +1,57 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include "BoyerMoore.h"
+
+#include <string.h>
+#include <stdio.h>
+
+#define MAX_BUFFER 512
+
+using namespace std;
+int main(int argc, char **argv)
+{
+	const char *filePath;
+	FILE *fpx;
+
+	char text[MAX_BUFFER];
+	char *pattern;
+	int patternLen = 0;
+	int line = 0;
+	bool find = false;
+
+	if (argc < 3)
+	{
+		cout << "Usage: " << argv[0] << " <FilePath> <Pattern>" << endl;
+		return 1;
+	}
+
+	filePath = argv[1];
+	pattern = argv[2];
+	patternLen = strlen(pattern);
+
+	fpx = fopen(filePath, "r");
+	if (fpx == NULL)
+	{
+		cout << "Can not open file " << filePath << endl;
+		return 1;
+	}
+
+	while (fgets(text, MAX_BUFFER, fpx) != NULL)
+	{
+		int pos = BoyerMoore(text, (int)strlen(text), pattern, patternLen, 0);
+		line += 1;
+		if (pos >= 0)
+		{
+			cout << "Line: " << line << " pos: " << pos << " : " << text << endl;
+			find = true;
+		}
+	}
+
+	if (!find)
+		cout << filePath << " has no pattern: " << pattern << endl;
+
+	fclose(fpx);
+	return 0;
+}
